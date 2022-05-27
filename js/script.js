0d8a08389f37124bec750cfc9ff8ed7dbf5a3da5
DzmitryMiankou <gmiankou@gmail.com>
2022-05-27 23:44:51 +0300

    chek

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
 `use script`
 document.addEventListener("DOMContentLoaded", () => {
-const dateToDay = document.querySelector('input[type="date"]').valueAsDate = new Date();;
-});
+const buttons = document.querySelector(`button`);
+
+const dateToDay = document.querySelector('input[type="date"]').valueAsDate = new Date();
+const d = new Date(dateToDay);
+
+
+
+ buttons.addEventListener("click", (event) => {
+        let target = event.target;
+        switch(target.id) {
+          case "kurs":
+            const date = document.forms["date"];
+            const em = date.elements["date"].value;
+            const sr = new Date(em);
+            console.log(d>sr);
+            break;
+        }
+      });
+      
+/*const chekDate = () => {
+    if (em == dateToDay) {
+        console.log(`hi`);
+    }
+
+
+}
+chekDate();*/
+
+
+
+});//Конец кода
