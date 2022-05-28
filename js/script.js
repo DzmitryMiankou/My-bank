7dbbdf0e33df4bb103e709297c7794c2515c73ab
DzmitryMiankou <gmiankou@gmail.com>
2022-05-28 11:32:44 +0300

    DateProcessing

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
 `use script`
-document.addEventListener("DOMContentLoaded", () => {
 const buttons = document.querySelector(`button`);
-
-const dateToDay = document.querySelector('input[type="date"]').valueAsDate = new Date();
-const d = new Date(dateToDay);
-
+let dateToDay = document.querySelector('input[type="date"]').valueAsDate = new Date ();
+let day = dateToDay.setHours(0, 0, 0, 0);
 
 
- buttons.addEventListener("click", (event) => {
-        let target = event.target;
-        switch(target.id) {
-          case "kurs":
-            const date = document.forms["date"];
-            const em = date.elements["date"].value;
-            const sr = new Date(em);
-            console.log(d>sr);
-            break;
-        }
-      });
-      
-/*const chekDate = () => {
-    if (em == dateToDay) {
-        console.log(`hi`);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const date = new DateProcessing(buttons, day);
+date.getButt();
+
+
+
+});//The end_________________________DOMContentLoaded______________________________________
+
+
+class DateProcessing {
+    constructor(button, day) {
+        this.button = button;
+        this.toDay = day;
     }
-
-
-}
-chekDate();*/
-
-
-
-});//Конец кода
+    getButt() {
+        this.button.addEventListener("click", (event) => {
+            let target = event.target;
+            switch(target.id) {
+                case "date__input"://Ввод курса
+                    this.chekDay();
+                    break;
+                }
+            });
+        }
+    chekDay() {
+        const date = document.forms["date"];
+        const inputDay = date.elements["date"].value;
+        const newDay = new Date(inputDay);
+        newDay.setHours(0, 0, 0, 0);
+        if(this.toDay < newDay.getTime()) {
+            document.querySelector('input[type="date"]').valueAsDate = new Date ();
+        }
+        if(this.toDay === newDay.getTime()) {
+            alert(`ok`);
+        }
+    }
+} //The end______________________________DateProcessing______________________________________
